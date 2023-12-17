88dfc673256401d08473360aaf3beea15715d508
pablowinter <pablo.winter@gmail.com>
2023-12-17 00:42:35 -0300

    Primer test

--- test.js
+++ test.js
@@ -1,11 +1,38 @@
-let numero=1;
-switch (numero){
-    case 1:
-        console.log("Paso 1");
-    case 2:
-        console.log("Paso 2");
-    case 3:
-        console.log("Paso 3");
-    case 4:
-        console.log("Paso 4");
+class Producto{
+    constructor(id,nombre){
+        this._id=id;
+        this._nombre=nombre;
+        this._precio=0.0;
+    }
+
+    get id(){
+        return this._id;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre=nombre;
+    }
+
 }
+
+const productos=[
+    new Producto(1,"Arroz"),
+    new Producto(1,"Yerba"),
+    new Producto(1,"Cafe"),
+    new Producto(1,"Fideos"),
+    new Producto(1,"Salsa de Tomate"),
+    new Producto(1,"Dulce de Leche"),
+    new Producto(1,"Azucar")
+]
+
+console.log(productos);
+console.clear();
+
+
+
+
+
