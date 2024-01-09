8eecb25f31b626af7bba6275abc6df1775235f46
pablowinter <pablo.winter@gmail.com>
2024-01-09 15:43:59 -0300

    Implementando carrito de compras

--- main.js
+++ main.js
@@ -34,7 +34,24 @@ class Producto{
         this.precio=precio;
         this.destacado=destacado;
     }
+
+    show (){
+        return `${this.nombre} - $ ${this.precio}`
+    }
 }
+
+class ItemCompra {
+    constructor(producto,cantidad){
+        this.producto=producto;
+        this.cantidad=cantidad;
+        this.importe=producto.precio*cantidad;
+    }
+    show(){
+        return `${this.producto.show()} - Cant. ${this.cantidad} - Importe $ ${this.importe}`
+    }
+}
+
+
 class FormaPago{
     constructor(codigo,nombre){
         this.codigo=codigo;
@@ -47,6 +64,35 @@ class Intereres{
         this.interes=interes;
     }
 }
+
+class Carrito{
+    constructor(){
+        this.items=[];
+        this.total=0.0;
+    }
+
+
+    //TODO: Ver si esta en el carrito
+    //refactorizar con items
+    addProduct(producto,cantidad){
+        /* this.total+=producto.precio*cantidad;
+        this.productos.push(producto); */
+    }
+
+    //TODO: Ver si esta en el carrito
+    //refactorizar con items
+    removeProduct(producto,cantidad){
+        //this.total-=producto.precio*cantidad;
+        //todo remover con splice
+    }
+
+    get roundedTotal(){
+        round(this.total,2);
+    }
+
+}
+
+
 
 //Base de Datos
 //Clientes
@@ -79,6 +125,8 @@ const intereses=[
     new Intereres(6,0.1),
     new Intereres(12,0.15)
 ]
+
+
 
 //Funciones de uso comun
 const sortAsc=(a,b)=>{
@@ -150,30 +198,59 @@ function login(intentos=1){
     }
 }
 
+function round(numero, digits){
+    let factor=Math.pow(10,digits);
+    
+    return Math.round(numero*factor)/factor;
+ 
+ }
+
+const productosShow = function(callback,valorInicial="") {
+    let cadena="";
+
+    cadena=this.reduce((a,b)=>a+=b.show() + "\n" ,valorInicial);
+    return cadena;
+
+}
+
 const formaBusqueda="Buscar por :\n1-Codigo\n2-Nombre\n3-Precio\n4-Todos";
+
+function findProductByCodigo(codigo,productos){
+    if (productos.length===0){
+        return null;
+    }else{
+        return productos.find(p=> p.codigo===codigo);
+    }
+}
+
 
 function productSearch(mensaje){    
     mensaje+=formaBusqueda;
     let input=numberInput(mensaje);
-    if (input===exitCode) return exitCode;
+    if (input===exitCode) return [];
     let productosFiltrados=[];
     if (input===1){
         input=numberInput("Ingrese el codigo a buscar");
-        if (input===exitCode) return exitCode;
-        productosFiltrados=productos.filter(p=> p.codigo===input);
-    }else if (input===2){
-        if (input===exitCode) return exitCode;
-        input=stringInput("Ingrese el nombre o parte del nombre a buscar");
-        while (input.length<3 && input!=exitCode){
-            alert("Debe ingresar al menos 3 caracteres");
+        if (input===exitCode) return [];
+        let producto=findProductByCodigo(input);
+        if (producto!=null){
+            productosFiltrados.push(producto);
         }
-        if (input===exitCode) return exitCode;
+    }else if (input===2){;
+        while ((input=stringInput("Ingrese el nombre o parte del nombre a buscar"))!=exitCode){
+            if (input.trim().length<4){
+                alert("Debe ingresar al menos 3 caracteres");
+            }else{
+                break;
+            }
+        }
+        if (input===exitCode) return [];
         productosFiltrados=productos.filter(p=> p.nombre.toLowerCase.includes(input));
     }else if (input===3){
         let precioDesde=numberInput("Ingrese el precio desde");
-        if (precioDesde===exitCode) return exitCode;
+        if (precioDesde===exitCode) return [];
         let precioHasta=numberInput("Ingrese el precio hasta");
-        if (precioHasta===exitCode) return exitCode;
+        if (precioHasta===exitCode) return [];
         productosFiltrados.filter(p=> p.precio>=precioDesde && p.precio<=precioHasta);
     }else{
         productosFiltrados=productos.slice();
@@ -185,7 +262,7 @@ function productSearch(mensaje){
         if (confirm("No se encontraron resultados\n¿Desea continuar")){
             return productSearch(mensaje);
         }else{
-            return exitCode;
+            return [];
         }
     }
 
@@ -206,7 +283,6 @@ function calcularMontoTotalEnCuotas(cuotas, valorCuota){
 }
 
 
-
 //Constantes
 const opcionesCuotas="1-2% Mensual.\n3-8% Mensual\n6-10% Mensual\n12-15% Mensual"
 //Codigo de fin de programa
@@ -214,35 +290,52 @@ const exitCode="cancel";
 
 //Configuracion inicial
 const productosDestacados=productos.filter(p=> p.destacado).sort((a,b)=>sortAsc(a.nombre,b.nombre));
-let productosDestacadosString=productosDestacados.reduce((a,b)=>a+=`${b.codigo}-${b.nombre}  $ ${b.precio}\n`,"Productos Destacados\n") 
-
+let productosDestacadosString=productosDestacados.productosShow=productosShow; 
+productosDestacados="Estos son los productos destacados de la semana\n" + productosDestacados.productosShow(this,"Productos Destacados\n");
 
 alert("Bienvenido a Pablo Winter Muebles");
 let input=0;
 while ((input=login())!=exitCode){
     let cliente=input;
-    alert(`Bienvenido ${cliente.nombre}`);
+    alert(`Bienvenido ${cliente.nombre}\n` + productosDestacados);
     let total=0;
     let totalParcial="";
-    let menuProductos="";
+    let carrito=null;
     let canceloCargaPedidos=false;
-    alert(productosDestacadosString);
-    while ((input=productSearch("Busqueda de Productos\n" + menuProductos + totalParcial))!=exitCode){
-        let producto=input;
-        //---
-        let codigoProducto=input;
-        alert(`Ud Selecciono el producto ${producto}`);
-        while ((input=numberInput("Ingrese la cantidad,cancelar para salir"))!=exitCode){
-            let cantidad=input;
-            if (cantidad>0){
-                let precio=buscarPrecio(codigoProducto);
-                total+=cantidad * precio;
-                break;
-            }else{
-                alert("La cantidad debe ser mayor a 0.");
-            }
+    let exit=false;
+    while (exit===false){
+        let productosEncontrados=productSearch("Busqueda de Productos");
+        if (carrito!=null && carrito.productos.length>0){
+
         }
-        totalParcial=`Total Parcial : $ ${total}`;
+        while ((input=numberInput("Ingrese el codigo del Producto"))!=exitCode){
+            let tmpProd;
+            if (productosEncontrados!=exitCode){
+                tmpProd=productosEncontrados;
+            }else{
+                tmpProd=productos;
+            }
+            //todo ver si se duplica
+            let producto=tmpProd.find(p=> p.codigo===input);
+
+            if (producto!=null){
+                alert(`Ud Selecciono el producto ${producto.show()}`);
+                while ((input=numberInput("Ingrese la cantidad,cancelar para salir"))!=exitCode){
+                    let cantidad=input;
+                    if (cantidad>0){
+                        if (carrito==null){
+                            carrito=new Carrito();
+                        }
+                        carrito.addProduct(producto,cantidad);
+                        break;
+                    }else{
+                        alert("La cantidad debe ser mayor a 0.");
+                    }
+                }
+                totalParcial=`Total Parcial : $ ${carrito.roundedTotal()}`;
+            }
+
+        }
     }
     if (total===0){
         alert("No se cargo ningun producto, volvera al menu inicial");
