6bf79a5aee223329965b58c110f8c92830271fa0
pablowinter <pablo.winter@gmail.com>
2024-01-09 09:36:53 -0300

    Creacion de Clases y llenado de arrays

--- main.js
+++ main.js
@@ -1,37 +1,61 @@
-//Datos
+//Clases
+class Cliente{
+    constructor(id,nombre){
+        this.id=id;
+        this.nombre=nombre;
+    }
+}
+class Producto{
+    constructor(codigo,nombre,precio){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+}
+class FormaPago{
+    constructor(codigo,nombre){
+        this.codigo=codigo;
+        this.nombre=nombre;
+    }
+}
+class Intereres{
+    constructor(cantCuotas,interes){
+        this.cantCuotas=cantCuotas;
+        this.interes=interes;
+    }
+}
+
+//Base de Datos
 //Clientes
-const numeroCliente1=10;
-const nombreCliente1="Juan Rodrigues";
-const numeroCliente2=20;
-const nombreCliente2="Carlos Acuña";
-const numeroCliente3=30;
-const nombreCliente3="Maria Garcia"
-const numeroCliente4=40;
-const nombreCliente4="Alejandra Gomrez";
-
+const clientes=[
+    new Cliente(10,"Juan Rodriguez"),
+    new Cliente(20,"Carlos Acuña"),
+    new Cliente(30,"Maria Garcia"),
+    new Cliente(40,"Alejandra Rodriguez")
+]
 //Productos
-const codigoProducto1=100;
-const nombreProducto1="Cama Beta Line 1 plaza";
-const precioProducto1=1500.30;
-const codigoProducto2=200;
-const nombreProducto2="Mesa de Luz de nogal";
-const precioProducto2=2300.40;
-const codigoProducto3=300;
-const nombreProducto3="Cama de 1 plaza de nogal Sweet Dreams 2 plazas";
-const precioProducto3=1000;
-const codigoProducto4=400;
-const nombreProducto4="Modular de Algarrobo con 3 puertas";
-const precioProducto4=3000.50;
-
+const productos=[
+    new Producto(100,"Cama Beta Line 1 plaza",3500),
+    new Producto(200,"Mesa de Luz de nogal",2300.40),
+    new Producto(300,"Cama de 1 plaza de nogal Sweet Dreams 2 plazas",1000),
+    new Producto(400,"Cama Beta Line 1 plaza"),
+    new Producto(400,"Modular de Algarrobo con 3 puertas",3000.50),
+    new Producto(500,"Cómoda Génova de 3 - 6 - 9 cajones"),
+]
 //Formas de Pago
-const pagoContado="CONTADO";
-const pagoCuotas="CUOTAS";
+const formasPago=[
+    new FormaPago(1,"CONTADO"),
+    new FormaPago(2,"CUOTAS")
+]
+//Intereses
+const intereses=[
+    new Intereres(1,0.02),
+    new Intereres(3,0.08),
+    new Intereres(6,0.1),
+    new Intereres(12,0.15)
+]
 
-//Cuotas
-const interes1Cuotas=0.02;
-const interes3Cuotas=0.08;
-const interes6Cuotas=0.1;
-const interes12Cuotas=0.15;
+
 const opcionesCuotas="1-2% Mensual.\n3-8% Mensual\n6-10% Mensual\n12-15% Mensual"
 
 //Codigo de fin de programa
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 class Producto{
-    constructor(id,nombre){
+    constructor(id,nombre,precio){
         this._id=id;
         this._nombre=nombre;
-        this._precio=0.0;
+        this.precio=precio;
     }
 
     get id(){
@@ -20,19 +20,67 @@ class Producto{
 }
 
 const productos=[
-    new Producto(1,"Arroz"),
-    new Producto(1,"Yerba"),
-    new Producto(1,"Cafe"),
-    new Producto(1,"Fideos"),
-    new Producto(1,"Salsa de Tomate"),
-    new Producto(1,"Dulce de Leche"),
-    new Producto(1,"Azucar")
+    new Producto(1,"Arroz",15),
+    new Producto(2,"Yerba",100),
+    new Producto(3,"Cafe",220),
+    new Producto(4,"Fideos",300),
+    new Producto(5,"Salsa de Tomate",250),
+    new Producto(6,"Dulce de Leche",1000),
+    new Producto(7,"Azucar",1500)
 ]
 
-console.log(productos);
-console.clear();
+/* 
+//SLICE
+const prodFiltrados=productos.slice(2,5);
+console.log("prodFiltrados")
+console.log(prodFiltrados); */
 
+//SPLICE
+/* console.log("splice")
+productos.splice(1,2);
+console.log(productos); */
 
+//INDEX OF
+/* let index=productos.map(e => e.nombre).indexOf("Arroz");
 
+console.log("Filtrar producto por IndexOf");
+console.log(productos[index]); */
 
+//INCLUDES
+/* let include=productos.map(e=> e.nombre).includes("Arroz");
+console.log("Filtrar producto por Includes");
+console.log("Arroz " + include); */
 
+//FIND
+/* let find=productos.find(e=> e.id===3);
+console.log(find); */
+
+//FILTER
+/* let productosFiltrados=productos.filter(p=> p.precio>200 && p.precio<1000);
+console.log(productosFiltrados); */
+
+//REDUCE
+/* const precios=[10,20,30,40];
+
+const suma=precios.reduce((a,b)=> a+=b,0);
+
+console.log(suma); */
+
+const nombres=["Juan","Pedro","Luis","Alejandra","Miguel"];
+const premios=["Auto","Departamento","Lavarropas","Celular"]
+
+const numerosAleatorios=(a)=>Math.floor(Math.random() * a);
+/* 
+for (let i=0;i<100;i++){
+    mensaje=`${nombres[numerosAleatorios(nombres.length)]} recibo el premio ${premios[numerosAleatorios(premios.length)]}`
+    console.log(mensaje);
+}
+ */
+
+/* for (let i=0;i<100;i++){
+    console.log(Math.round(Math.random() * 5));
+}
+ */
+let s="123";
+let s2=s.padStart(8,"0");
+console.log(String(6).padStart(3,"0"));
